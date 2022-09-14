f237d7f3b2329e2bf8331fd2d3177e1d5810fef7
Anangery <39629371+TSCodes@users.noreply.github.com>
2022-09-14 08:06:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const front = document.getElementById('front');
+const submit = document.getElementById('submit');
+submit.addEventListener("click", "function")
+
+function submit(){
+    alert(front.value);
+}
