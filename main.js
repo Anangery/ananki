80463947ae11397796b3fbcffccab34c5a59cb07
Anangery <39629371+TSCodes@users.noreply.github.com>
2022-09-15 13:48:24 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-const front = document.getElementById('front');
-const submit = document.getElementById('submit');
-submit.addEventListener("click", "function")
+let front = document.getElementById("front");
+let back = document.getElementById("back");
+let content = [];
+
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.key === "Enter"){
+        submit()
+        e.preventDefault();
+    }
+});
 
 function submit(){
-    alert(front.value);
+    content.push({
+        key: front.value,
+        value: back.value
+    });
+    console.log(content);
+    front.value = null;
+    back.value = null;
+    front.focus();
+}
+
+function done() {
+    console.log("done");
 }
